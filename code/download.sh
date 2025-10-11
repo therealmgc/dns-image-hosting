d87854428242eb73bbc3304e76095d3bb5a56e37
devops <devops@localhost.it>
2025-10-11 17:34:36 +0200

    first commit

---
+++ code/download.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+#
+# No input params
+#
+
+MYDIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+source "${MYDIR}/env.conf" || (echo "FATAL: Configuration file not found"; exit 1)
+
+# Uncomment following line to get log
+# exec > >(tee "${MYDIR}/${_ME}".log) 2>&1
+
+
+# __MAIN__
+
+# Get number of chunks
+count=$( dig @${_DNSHOST} +short TXT ${_SUBCOUNT} | tr -d '" \n' )
+width=${#count}   
+echo "chunk to be restored: $count; using $width digitrms names"
+
+# Cleanup
+> ${MYDIR}/image.${_ENC}
+> ${MYDIR}/image.bin  
+if [ -d ${MYDIR}/${_WORKDIRDWN}/ ]; then  
+   \rm -fr ${MYDIR}/${_WORKDIRDWN}
+fi    
+mkdir -p ${MYDIR}/${_WORKDIRDWN}/.
+
+# Start the spinner
+echo -e "\nDownloading chunks... "
+spinner "> " &
+spinner_pid=$!
+
+# Restore from chunks 
+for ((ii=0; ii<count; ii++)); do
+   SUB="$(printf "%s%0${width}d" "${_PREFIX}" "$ii").${_DOMAIN}"
+   txt=$( dig @${_DNSHOST} +short TXT $SUB | tr -d '" \n' )
+   echo $txt >> ${MYDIR}/image.${_ENC}
+   echo -n $txt > ${MYDIR}/${_WORKDIRDWN}/$SUB 
+done 
+
+# Stop the spinner
+kill "$spinner_pid" 2>/dev/null
+wait "$spinner_pid" 2>/dev/null
+
+# Decode
+cat ${MYDIR}/image.${_ENC} |  base64 -d > ${MYDIR}/image.bin                    
+ret=$? 
+\rm -f ${MYDIR}/image.${_ENC}
+
+echo
+echo "Exit value is $ret"
+echo
+exit $(( ret % 256 ))
+
+
+
+# TODO: runcheck.sh that does the followindìg:
+# fare file di configurazione di cui faccio source in upload/download/test
+#
+# $> for f in $(ls -1 b64*); do echo $f; diff -w ./$f ./run/$f.labyrinth.lab; done
---
+++ code/upload.sh
@@ -0,0 +1,108 @@
+#!/bin/bash
+
+#
+# $1: bin file
+#
+
+# Check if an argument was provided
+if [ $# -lt 1 ] || [ ! -f "$1" ]; then
+   echo "Usage: $0 <filename>"
+   exit 1
+fi
+
+in_file=$1
+echo "Input file is ${in_file}"
+
+MYDIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+source "${MYDIR}/env.conf" || (echo "FATAL: Configuration file not found"; exit 1)
+
+# Uncomment following line to get log
+# exec > >(tee "${MYDIR}/${_ME}".log) 2>&1
+
+
+# __MAIN__
+
+# Cleanup
+if [ -d ${MYDIR}/${_WORKDIRUP}/ ]; then 
+  \rm -fr ${MYDIR}/${_WORKDIRUP}
+fi
+mkdir -p ${MYDIR}/${_WORKDIRUP} 
+
+# Copy input file into temporary dir
+\cp -fa ${in_file} ${MYDIR}/${_WORKDIRUP}/$(basename $in_file)
+
+# Encode - note -w0 option to have a single line
+in_file64="${MYDIR}/${_WORKDIRUP}/$(basename $in_file).${_ENC}"
+cat $in_file | base64 -w0 > $in_file64
+
+# cd into temporary dir
+pushd "${MYDIR}/${_WORKDIRUP}" > /dev/null || { echo "Directory not found: ${MYDIR}/${_WORKDIRUP}"; exit 1; }
+
+# Split encoded file according to CHUNCKSIZE
+split -b ${_CHUNKSIZE} $in_file64 ${_PREFIX}
+
+# Get number of chunk
+num_chunks=$( ls -1 ${_PREFIX}* | wc -l ) 
+width=${#num_chunks}
+echo "Number of chunks: $num_chunks"
+
+# Rename chunks 
+echo "Renaming chunks with $width digits progression"
+for f in $( ls -1 ${_PREFIX}* ) ; do
+  mv "$f" "$(printf "%s%0${width}d" "${_PREFIX}" "$n")"
+  ((n++))
+done
+
+# Get auth token 
+TOKEN=$(curl -s "http://${_DNSHOST}:${_DNSPORT}/api/user/login?user=${_DNSUSR}&pass=${_DNSPASS}" | jq -r .token)         
+echo "TOKEN=$TOKEN"    
+     
+# Delete and create domain
+curl -s --data-urlencode "token=$TOKEN" http://${_DNSHOST}:${_DNSPORT}/api/zones/delete?zone=${_DOMAIN} &>/dev/null
+sleep 1 
+curl -s --data-urlencode "token=$TOKEN" http://${_DNSHOST}:${_DNSPORT}/api/zones/create?zone=${_DOMAIN}&type=Primary
+
+# Start the spinner 
+echo -e "Uploading chunks... "
+spinner "> " &
+spinner_pid=$!
+
+# Upload chunks
+first=1
+for mychunk in $( ls -1 "${_PREFIX}"* ); do
+   SUB="${mychunk}.${_DOMAIN}"
+   curl -s --data-urlencode "text=$(cat $mychunk)" "http://${_DNSHOST}:${_DNSPORT}/api/zones/records/add?token=$TOKEN&domain=$SUB&type=TXT&ttl=60" > /dev/null
+   sleep 0.2
+done 
+
+# Store count with a special name to be used to retrieve chunks 
+curl -s --data-urlencode "text=$(echo $num_chunks)" \
+     "http://${_DNSHOST}:${_DNSPORT}/api/zones/records/add?token=$TOKEN&domain=${_SUBCOUNT}&type=TXT&ttl=60" > /dev/null
+
+# return to main directory 
+popd > /dev/null
+
+# Stop the spinner
+kill "$spinner_pid" 2>/dev/null
+wait "$spinner_pid" 2>/dev/null
+
+echo
+exit
+
+# At this point DNS server stores TXT records as:
+#
+#
+#
+#b64-000	TXT	60
+#(1m)	iVBORw0KGgoAAAANSUhEUgAAAoAAAAHfCAMAAAD3I5muAAADAFBMVEUAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
+#
+#b64-001	TXT	60
+#(1m)	AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
+#
+#[...]
+#
+#b64-count	TXT	60
+#(1m)	780   <- This is the total number of chunks
+#
+
+
